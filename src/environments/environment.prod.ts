7472f3ec4183b7e703cb539861597bc3131a8529
Anderson Fernandes <andersonxf@gmail.com>
2020-05-07 17:02:07 -0300

    alterado environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://andfinanca-api.herokuapp.com/',
+  apiUrl: 'https://andfinanca-api.herokuapp.com',
 
-  tokenWhitelistedDomains: [ new RegExp('andfinanca-api.herokuapp.com/') ],
+  tokenWhitelistedDomains: [ new RegExp('andfinanca-api.herokuapp.com') ],
   tokenBlacklistedRoutes: [ new RegExp('\/oauth\/token') ]
 };
